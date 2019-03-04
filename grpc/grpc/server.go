9237d2be49ba61c3b4d4be53863113b9d0e6e438
Riverside <wangyb65@gmail.com>
2019-03-04 16:57:59 +0800

    change github.com/gin-gonic/gin/examples/grpc/pb to github.com/gin-gonic/examples/grpc/pb
    becouse All gin examples has moved as alone repository to github.com/gin-gonic/examples

--- grpc/grpc/server.go
+++ grpc/grpc/server.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/gin-gonic/gin/examples/grpc/pb"
+	pb "github.com/gin-gonic/examples/grpc/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"

2a6477130d67ce3369c23f3677c52dba6abe61a0
cncal <flycalvin@qq.com>
2019-04-09 22:18:57 +0800

    enhancement #8: add file binding example (#10)

---
+++ file-binding/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"mime/multipart"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BindFile struct {
+	Name  string                `form:"name" binding:"required"`
+	Email string                `form:"email" binding:"required"`
+	File  *multipart.FileHeader `form:"file" binding:"required"`
+}
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("/", "./public")
+	router.POST("/upload", func(c *gin.Context) {
+		var bindFile BindFile
+
+		// Bind file
+		if err := c.ShouldBind(&bindFile); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("err: %s", err.Error()))
+			return
+		}
+
+		// Save uploaded file
+		file := bindFile.File
+		dst := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, dst); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, bindFile.Name, bindFile.Email))
+	})
+	router.Run(":8080")
+}

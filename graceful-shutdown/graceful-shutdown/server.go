e5ac4fb8b206f4afc75c30c22ca979803c57c800
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-05-10 14:48:53 +0800

    Update server.go
    closed #11

--- graceful-shutdown/graceful-shutdown/server.go
+++ graceful-shutdown/graceful-shutdown/server.go
@@ -46,11 +46,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
+		log.Fatal("Server Shutdown: ", err)
 	}
-	// catching ctx.Done(). timeout of 5 seconds.
-	<-ctx.Done()
-	log.Println("timeout of 5 seconds.")
 
 	log.Println("Server exiting")
 }

ad4aeab9139e0e13a653156f5b1cd45fea5cd517
Rafael Ferreira <raf.ferreira@gmail.com>
2019-12-31 09:38:39 -0500

    including runtime for go113

---
+++ app-engine/go11x/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	// Starts a new Gin instance with no middle-ware
+	r := gin.New()
+
+	// Define handlers
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!")
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Listen and serve on defined port
+	log.Printf("Listening on port %s", port)
+	r.Run(":" + port)
+}

62e9b512341afc5d81858dd663467b63f63c1390
Igor H. Vieira <zignd.igor@gmail.com>
2020-03-21 19:47:37 -0300

    Improved the comments and one of the log messages
    Improved the comments and one of the log messages to make it clearer for beginners.

--- graceful-shutdown/graceful-shutdown/server.go
+++ graceful-shutdown/graceful-shutdown/server.go
@@ -26,8 +26,9 @@ func main() {
 		Handler: router,
 	}
 
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
 	go func() {
-		// service connections
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
@@ -35,19 +36,21 @@ func main() {
 
 	// Wait for interrupt signal to gracefully shutdown the server with
 	// a timeout of 5 seconds.
-	quit := make(chan os.Signal, 1)
+	quit := make(chan os.Signal)
 	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
 	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shutdown Server ...")
+	log.Println("Shuting down server...")
 
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown: ", err)
+		log.Fatal("Server forced to shutdown:", err)
 	}
-
+	
 	log.Println("Server exiting")
 }

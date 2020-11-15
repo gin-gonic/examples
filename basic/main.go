22b982b57d829d0a2759cba0dfc93d4fc297505d
JodeZer <jeffery1993@hotmail.com>
2020-11-15 22:04:57 +0800

    example notes for authorization

--- basic/main.go
+++ basic/main.go
@@ -41,6 +41,15 @@ func setupRouter() *gin.Engine {
 		"manu": "123", // user:manu password:123
 	}))
 
+	/* example curl for /admin with basicauth header
+	   Zm9vOmJhcg== is base64("foo:bar")
+
+		curl -X POST \
+	  	http://localhost:8080/admin \
+	  	-H 'authorization: Basic Zm9vOmJhcg==' \
+	  	-H 'content-type: application/json' \
+	  	-d '{"value":"bar"}'
+	*/
 	authorized.POST("admin", func(c *gin.Context) {
 		user := c.MustGet(gin.AuthUserKey).(string)
 

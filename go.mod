2fb6967709ba6b5a7a5c6218fb99f6e084d1047a
Bo-Yi Wu <appleboy.tw@gmail.com>
2021-05-22 08:07:14 +0800

    chore(graceful-shutdown): add NotifyContext example
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- go.mod
+++ go.mod
@@ -6,9 +6,9 @@ require (
 	github.com/dustin/go-broadcast v0.0.0-20171205050544-f664265f5a66
 	github.com/fatih/color v1.9.0
 	github.com/gin-gonic/autotls v0.0.0-20190406003154-fb31fc47f521
-	github.com/gin-gonic/gin v1.5.0
-	github.com/go-playground/validator/v10 v10.1.0
-	github.com/golang/protobuf v1.3.2
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/validator/v10 v10.4.1
+	github.com/golang/protobuf v1.3.3
 	github.com/jessevdk/go-assets v0.0.0-20160921144138-4f4301a06e15
 	github.com/manucorporat/stats v0.0.0-20180402194714-3ba42d56d227
 	github.com/newrelic/go-agent v2.7.0+incompatible
@@ -16,7 +16,7 @@ require (
 	github.com/thinkerou/favicon v0.1.0
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/ratelimit v0.1.0
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/grpc v1.25.1
---
+++ graceful-shutdown/graceful-shutdown/notify-with-context/server.go
@@ -0,0 +1,56 @@
+// build go1.16
+
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		time.Sleep(10 * time.Second)
+		c.String(http.StatusOK, "Welcome Gin Server")
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}

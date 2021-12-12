a61976f983ec4ff413df10f451aecd418418d142
stepbystep2 <770954908@qq.com>
2021-12-12 13:33:33 +0800

    fmt.Sprintf() is redundant (#63)

--- upload-file/multiple/main.go
+++ upload-file/multiple/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"path/filepath"
 
@@ -20,7 +19,7 @@ func main() {
 		// Multipart form
 		form, err := c.MultipartForm()
 		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
 			return
 		}
 		files := form.File["files"]
@@ -28,12 +27,12 @@ func main() {
 		for _, file := range files {
 			filename := filepath.Base(file.Filename)
 			if err := c.SaveUploadedFile(file, filename); err != nil {
-				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+				c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
 				return
 			}
 		}
 
-		c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email))
+		c.String(http.StatusOK, "Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email)
 	})
 	router.Run(":8080")
 }

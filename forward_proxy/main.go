d9e5ae3d54987ced41d0dbd8abc87fe4060b988f
Wang Qi <wangmarkqi@users.noreply.github.com>
2024-02-25 09:57:02 +0800

    chore(proxy): Add forward proxy (#134)
    * add forward proxy
    
    * Del.idea

---
+++ forward_proxy/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(ForwardMid)
+
+	//Create a catchall route
+	r.Any("/*proxyPath", Reverse)
+
+	log.Info("启动http")
+	r.Run(":8888")
+}
+
+func ForwardMid(c *gin.Context) {
+	// !!! adapt to your request header set
+	if v, ok := c.Request.Header["Forward"]; ok {
+		if v[0] == "ok" {
+			resp, err := http.DefaultTransport.RoundTrip(c.Request)
+			if err != nil {
+				http.Error(c.Writer, err.Error(), http.StatusServiceUnavailable)
+				c.Abort()
+				return
+			}
+			defer resp.Body.Close()
+			copyHeader(c.Writer.Header(), resp.Header)
+			c.Writer.WriteHeader(resp.StatusCode)
+			io.Copy(c.Writer, resp.Body)
+			c.Abort()
+			return
+		}
+	}
+
+	c.Next()
+}
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+func Reverse(c *gin.Context) {
+	remote, _ := url.Parse("http://xxx.xxx.xxx")
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+	}
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
